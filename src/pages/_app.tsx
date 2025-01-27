0f30b59fa9b168fe4cb89f8c676f9aa494966c88
Diaz Permana <051511515@ecampus.ut.ac.id>
2025-01-28 00:23:36 +0700

    Add pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,13 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-
-import { config } from '../wagmi';
+import { wagmiConfig } from '../config/wagmi';
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={client}>
         <RainbowKitProvider>
           <Component {...pageProps} />
